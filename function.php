f4ca61398860d0a3de91c23292e5c389b66244bb
Fabian M <90083123+FabienClarke@users.noreply.github.com>
2023-09-07 09:44:22 +0800

    Update function.php

--- function.php
+++ function.php
@@ -5,8 +5,7 @@ session_start();
 $conn = mysqli_connect("localhost", "root", "", "asset_management");
 
 //Alter or Update the Database
-if(isset($_POST['update'])){
-    $reconciliation_identity = $_POST['reconciliaton_identity'];
+if(isset($_POST['updatedatabase'])){
     $serial_number = $_POST['serial_number'];
     $name = $_POST['name'];
     $status = $_POST['status'];
@@ -17,9 +16,8 @@ if(isset($_POST['update'])){
     $tier_1 = $_POST['tier_1'];
     $tier_2 = $_POST['tier_2'];
     $tier_3 = $_POST['tier_3'];
-    $received_date = $_POST['received_date'];
-    $installation_date = $_POST['istallation_date'];
     $modified_date = $_POST['modified_date'];
+    $modified_by = $_POST['modified_by'];
     $site_group = $_POST['site_group'];
     $asset_site = $_POST['asset_site'];
     $building = $_POST['building'];
@@ -48,6 +46,18 @@ if(isset($_POST['update'])){
     $tag_number = $_POST['tag_number'];
     $siegel_normalized = $_POST['siegel_normalized'];
     $siegel_c = $_POST['siegel_c'];
+    $phm_status = $_POST['phm_status'];
+    $pc_new_model = $_POST['pc_new_model'];
+    $company = $_POST['company'];
+    $phase = $_POST['phase'];
+
+    $update = mysqli_query($conn, "update asset_data set name='$name', status='$status', short_description='$short_description', supplier_name='$supplier_name', model_number='$model_number', asset_id='$asset_id', tier_1='$tier_1', tier_2='$tier_2', tier_3='$tier_3', site_group='$site_group', asset_site='$asset_site', building='$building', floor_room='$floor_room', people_relationship_status='$people_relationship_status', site='$site', psa_code='$psa_code', psa_site_name='$psa_site_name', organization='$organization', department='$department', remedy_login_id='$remedy_login_id', full_name='$full_name', entity_relationship_status='$entity_relationship_status', div='$div', dept='$dept', support_group_supported_by_relationship_status='$support_group_supported_by_relationship_status', supported_by_organization='$supported_by_organization', supported_by_support_group='$supported_by_support_group', support_group_managed_by_relationship_status='$support_group_managed_by_relationship_status', managed_by_organization='$managed_by_organization', managed_by_support_group='$managed_by_support_group', normalized_entity='$normalized_entity', organization_merged='$organization_merged', department_merged='$department_merged', description='$description', tag_number='$tag_number', siegel_normalized='$siegel_normalized', siegel_c='$siegel_c', phm_status='$phm_status', pc_new_model='$pc_new_model', company='$company', phase='$phase' where serial_number='$serial_number'");
+    if($update){
+        header('location:edit.php');
+    } else {
+        header('location:export.php');
+    }
+
 }
 
 ?>
