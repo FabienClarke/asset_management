3d06dd52ccab656039892f1d6f92a7575205a227
Fabian M <90083123+FabienClarke@users.noreply.github.com>
2023-09-18 02:16:57 +0800

    Update function.php

--- function.php
+++ function.php
@@ -4,8 +4,37 @@ session_start();
 //Connect to Database
 $conn = mysqli_connect("localhost", "root", "", "asset_management");
 
+//register
+if (isset($_POST['register'])){
+    $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+    $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = $_POST['password'];
+
+    $insert_register = "INSERT INTO users (first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$password')";
+    $register = mysqli_query($conn, $insert_register);
+
+    if($register){
+        header('location:login.php');
+    } else {
+        echo "Registration failed: " . mysqli_error($conn);
+    }
+}
+
 //Edit or Update the Database
 if(isset($_POST['update'])){
+    $email = $_SESSION['email'];
+
+    $query = "SELECT first_name, last_name FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $query);
+
+    $row = mysqli_fetch_assoc($result);
+    $full_name = $row['first_name'] . ' ' . $row['last_name'];
+    $editor = $full_name;
+    
+    $inserthistoryquery = "INSERT INTO history (edit_date, editor) VALUES (NOW(), '$editor')";
+    $inserthistory = mysqli_query($conn, $inserthistoryquery);
+
     $serial_number = $_POST['serial_number'];
     $name = $_POST['name'];
     $status = $_POST['status'];
@@ -49,13 +78,12 @@ if(isset($_POST['update'])){
     $company = $_POST['company'];
     $phase = $_POST['phase'];
     
-    $update = mysqli_query($conn, "update asset_data set name='$name', status='$status', short_description='$short_description', supplier_name='$supplier_name', model_number='$model_number', asset_id='$asset_id', tier_1='$tier_1', tier_2='$tier_2', tier_3='$tier_3', site_group='$site_group', asset_site='$asset_site', building='$building', floor_room='$floor_room', people_relationship_status='$people_relationship_status', site='$site', psa_code='$psa_code', psa_site_name='$psa_site_name', organization='$organization', department='$department', remedy_login_id='$remedy_login_id', full_name='$full_name', entity_relationship_status='$entity_relationship_status', div='$div', dept='$dept', support_group_supported_by_relationship_status='$support_group_supported_by_relationship_status', supported_by_organization='$supported_by_organization', supported_by_support_group='$supported_by_support_group', support_group_managed_by_relationship_status='$support_group_managed_by_relationship_status', managed_by_organization='$managed_by_organization', managed_by_support_group='$managed_by_support_group', normalized_entity='$normalized_entity', organization_merged='$organization_merged', department_merged='$department_merged', description='$description', tag_number='$tag_number', siegel_normalized='$siegel_normalized', siegel_c='$siegel_c', phm_status='$phm_status', pc_new_model='$pc_new_model', company='$company', phase='$phase' where serial_number='$serial_number'");
-    if($update){ 
+    $serial_number = str_replace(';', '', $serial_number);
+    $updatefunc = "UPDATE asset_data SET name='$name', status='$status', short_description='$short_description', supplier_name='$supplier_name', model_number='$model_number', asset_id='$asset_id', tier_1='$tier_1', tier_2='$tier_2', tier_3='$tier_3', site_group='$site_group', asset_site='$asset_site', building='$building', floor_room='$floor_room', people_relationship_status='$people_relationship_status', site='$site', psa_code='$psa_code', psa_site_name='$psa_site_name', organization='$organization', department='$department', remedy_login_id='$remedy_login_id', full_name='$full_name', entity_relationship_status='$entity_relationship_status', `div`='$div', dept='$dept', support_group_supported_by_relationship_status='$support_group_supported_by_relationship_status', supported_by_organization='$supported_by_organization', supported_by_support_group='$supported_by_support_group', support_group_managed_by_relationship_status='$support_group_managed_by_relationship_status', managed_by_organization='$managed_by_organization', managed_by_support_group='$managed_by_support_group', normalized_entity='$normalized_entity', organization_merged='$organization_merged', department_merged='$department_merged', description='$description', tag_number='$tag_number', siegel_normalized='$siegel_normalized', siegel_c='$siegel_c', phm_status='$phm_status', pc_new_model='$pc_new_model', company='$company', phase='$phase' WHERE serial_number='$serial_number'";
+    $update = mysqli_query($conn, $updatefunc);
+    if($update){
         header('location:edit.php');
-    } else {
-        header('location:export.php');
     }
-
 }
 
 ?>
